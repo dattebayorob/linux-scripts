f2c590654a59104fb2113306a6f28324d12847ba
dattebayorob <robson.william65@gmail.com>
2019-01-09 00:39:32 -0300

    Polybar: Install fonts

--- polybar/install.sh
+++ polybar/install.sh
@@ -1,4 +1,6 @@
-#Polybar dependencies
+#!/bin/bash
+
+Polybar dependencies
 apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev\
     libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen\
     xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev\
@@ -9,3 +11,10 @@ cd polybar/build
 cmake ..
 make install
 cd ../../
+
+#Fonts install
+fontPath="usr/share/fonts/TTF"
+if [ ! -d /$fontPath ];then
+  mkdir /$fontPath
+fi
+cp $fontPath/* /$fontPath/
