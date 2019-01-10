83ae900a7a00e46f473f391cbccdede3e571bad2
dattebayorob <robson.william65@gmail.com>
2019-01-10 19:44:43 -0300

    Postman: Refactoring

--- postman/install.sh
+++ postman/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#dependencies
-#sudo apt-get install gconf-service gconf-service-backend gconf2-common libgconf-2-4 -y > /dev/null
+source '../utils/checkUser.sh'
 
-postmanPath="Documentos/packages/"
+checkUser $1
+
+## dependencies
+apt-get install gconf-service gconf-service-backend gconf2-common libgconf-2-4 -y > /dev/null
+
+## constants
+postmanPath="/home/${1}/Documentos/packages/"
 postmanPackage=$(ls Postman* | grep .tar.gz)
-mkdir -p /home/$USER/$postmanPath}
-tar -xf $postmanPackage -C /home/$USER/$postmanPath
-sudo cp /home/$USER/${postmanPath}/Postman/app/resources/app/assets/icon.png /usr/share/icons/hicolor/48x48/apps/postman.png
-sudo cp postman.desktop /usr/share/applications
-chmod +x /home/$USER/${postmanPath}Postman/Postman
+mkdir -p $postmanPath
+tar -xf $postmanPackage -C $postmanPath
+cp ${postmanPath}Postman/app/resources/app/assets/icon.png /usr/share/icons/hicolor/48x48/apps/postman.png
+cp postman.desktop /usr/share/applications
+chown -R $1:$1 ${postmanPath}
+chmod +x ${postmanPath}Postman/Postman
