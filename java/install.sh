42d4242f09fc6cd42a17ed9b88c842b4746981ef
dattebayoRob <robson.william65@gmail.com>
2019-02-25 15:45:29 +0000

    Revert "Java: No apt on arch"
    This reverts commit ba77502d5a6c5e0cbe48618f6c59f3752c0c87d3.

--- java/install.sh
+++ java/install.sh
@@ -31,6 +31,7 @@ JdkVersion(){
 SetupAmbient
 JdkVersion $arch
 echo "Instalando: "$jdk
+apt-get remove sun-java6*
 tar -zxf $arch/$(ls ${arch}) -C /usr/lib/jvm
 ln -s /usr/lib/jvm/$jdk /usr/lib/jvm/java-8-oracle
 update-alternatives --remove-all java
