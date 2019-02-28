89de7da9e4b153cf670cda9baea81634e0603bd0
dattebayoRob <robson.william65@gmail.com>
2019-02-28 13:14:55 -0300

    Misc changes on script (Settings for X11)

--- arch/install.sh
+++ arch/install.sh
@@ -2,12 +2,10 @@
 
 USER_DIRS="Desktop Documents Pictures Videos Downloads"
 
+### SETUP SYSTEM
 # INSTALL BASE PACKAGES
 APPS=$(cat apps.txt | sed ':a;N;$!ba;s/\n/ /g')
 pacman -Sy --noconfirm $APPS
-
-# SETUP hostname
-echo dtb-arch > /etc/hostname
 
 # SETUP LANGUAGE
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
@@ -15,12 +13,6 @@ locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 rm -f /etc/localtime
 ln -s /usr/share/zoneinfo/America/Fortaleza > /etc/localtime
-
-# SETUP USERS
-useradd -m -G wheel -s /bin/bash administrador
-echo "administrador:$ENV_PASSWORD" | chpasswd
-echo "root:$ENV_PASSWORD" | chpasswd
-sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
 # SETUP GRUB
 grub-install /dev/sda
@@ -30,7 +22,23 @@ mkinitcpio -p linux
 # SETUP NETWORK
 systemctl enable wicd.service
 
-# SETUP BASIC USER SETTINGS
+### SETUP SYSTEM SETTINGS
+# hostname
+echo dtb-arch > /etc/hostname
+# pt-br key on X11
+cp -rf settings/10-evdev.conf /etc/X11/xorg.conf.d/
+# settings for touchpad using libinput
+cp -rf settings/30-touchpad.conf /etc/X11/xorg.conf.d/
+chmod 644 /etx/X11/xorg.conf.d
+
+### SETUP USER
+# Useradd and user settings
+useradd -m -G wheel -s /bin/bash administrador
+echo "administrador:$ENV_PASSWORD" | chpasswd
+echo "root:$ENV_PASSWORD" | chpasswd
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+# Basic user settings
 cp -f /etc/skel/.* /home/administrador/
 echo exec openbox-session > /home/administrador/.xinitrc
 for dir in $USER_DIRS; do
