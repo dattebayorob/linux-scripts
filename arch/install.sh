57a5295d071fa1117cb7d0b0723296265f4eb647
dattebayoRob <robson.william65@gmail.com>
2019-02-28 13:27:52 -0300

    Setup language for console

--- arch/install.sh
+++ arch/install.sh
@@ -13,6 +13,8 @@ locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 rm -f /etc/localtime
 ln -s /usr/share/zoneinfo/America/Fortaleza > /etc/localtime
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+echo "localectl set-keymap --no-convert br-abnt2" >> /etc/bash.bashrc
 
 # SETUP GRUB
 grub-install /dev/sda
