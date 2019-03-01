5099587e345b614c5ea7803f9c7b5f027a31d422
dattebayoRob <robson.william65@gmail.com>
2019-03-01 00:21:10 +0000

    Dont prompt anything

--- arch/packages/packageInstall.sh
+++ arch/packages/packageInstall.sh
@@ -7,10 +7,10 @@ packageInstall(){
 	pkgGit=https://aur.archlinux.org/${pkg}.git
 	if echo "Cloning git repository from ${pkgGit}" && git clone $pkgGit ${dir}${pkg} > /dev/null 2>&1; then
 		cd ${dir}${pkg}
-		makepkg -Acs
-		echo $ENV_PASSWORD | sudo -S pacman -U --noconfirm ${pkg}*pkg.tar.xz
+		yes | makepkg -Acs
+		sudo pacman -U --noconfirm ${pkg}*pkg.tar.xz
 		cd ../../
-		echo $ENV_PASSWORD | sudo -S rm -r packages/$pkg
+		rm -rf packages/$pkg
 	else
 		echo "The git repository cant be reached"
 	fi
